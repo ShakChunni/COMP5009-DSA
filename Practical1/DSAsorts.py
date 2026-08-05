38deb1a0a2782073a78dcf681d773ae290a89ba5
ShakChunni <ashfaq601230@gmail.com>
2026-08-05 17:46:51 +0800

    Implement sorting algorithms and add performance report

--- Practical1/DSAsorts.py
+++ Practical1/DSAsorts.py
@@ -5,29 +5,55 @@
 #
 
 def bubbleSort(A):
-    passNum = 0
-    sortedArray = False
+    pass_number = 0
+    is_sorted = False
 
-    while passNum < A.size - 1 and not sortedArray:
-        sortedArray = True
-        currentIdx = 0
+    while pass_number < A.size - 1 and not is_sorted:
+        is_sorted = True
+        current_index = 0
 
-        while currentIdx < A.size - passNum - 1:
-            if A[currentIdx] > A[currentIdx + 1]:
-                temp = A[currentIdx]
-                A[currentIdx] = A[currentIdx + 1]
-                A[currentIdx + 1] = temp
-                sortedArray = False
+        while current_index < A.size - pass_number - 1:
+            if A[current_index] > A[current_index + 1]:
+                temp = A[current_index]
+                A[current_index] = A[current_index + 1]
+                A[current_index + 1] = temp
+                is_sorted = False
 
-            currentIdx += 1
+            current_index += 1
 
-        passNum += 1
+        pass_number += 1
 
 def insertionSort(A):
-    ...
+    current_position = 1
+
+    while current_position < A.size:
+        scan_index = current_position
+
+        while (scan_index > 0 and A[scan_index - 1] > A[scan_index]):
+            temp = A[scan_index]
+            A[scan_index] = A[scan_index - 1]
+            A[scan_index - 1] = temp
+            scan_index -= 1
+
+        current_position += 1
 
 def selectionSort(A):
-    ...
+    current_position = 0
+
+    while current_position < A.size - 1:
+        minimum_index = current_position
+        scan_index = current_position + 1
+
+        while scan_index < A.size:
+            if A[scan_index] < A[minimum_index]:
+                minimum_index = scan_index
+
+            scan_index += 1
+
+        temp = A[current_position]
+        A[current_position] = A[minimum_index]
+        A[minimum_index] = temp
+        current_position += 1
 
 def mergeSort(A):
     """ mergeSort - front-end for kick-starting the recursive algorithm
@@ -50,4 +76,3 @@ def quickSortRecurse(A, leftIdx, rightIdx):
 
 def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
     ...
-
